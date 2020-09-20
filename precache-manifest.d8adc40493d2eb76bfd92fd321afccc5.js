4c9023c14c8d561bfcf9bd7e92170bc431c0fe3c
Kakha1998 <kakha.gujejiani.1@iliauni.edu.ge>
2020-09-20 09:54:03 +0400

    Updates

---
+++ precache-manifest.d8adc40493d2eb76bfd92fd321afccc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4654beac669ff210af66b96f7ac1bc59",
+    "url": "/Promodo-Clock/index.html"
+  },
+  {
+    "revision": "362280d78a2b90c93548",
+    "url": "/Promodo-Clock/static/css/main.ad896165.chunk.css"
+  },
+  {
+    "revision": "0ef032cac6b88d982ff4",
+    "url": "/Promodo-Clock/static/js/2.da325554.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Promodo-Clock/static/js/2.da325554.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "362280d78a2b90c93548",
+    "url": "/Promodo-Clock/static/js/main.03c046a3.chunk.js"
+  },
+  {
+    "revision": "652aafaa52ad09ae0109",
+    "url": "/Promodo-Clock/static/js/runtime-main.177048b3.js"
+  }
+]);
